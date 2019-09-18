bf520710d03f36cc60e1f2a60a5098a244b910fa
Nicolai Ellebæk Bjerg <nico485g@student.eadania.dk>
2019-09-18 15:23:45 +0200

    Base of Login_Middleware solution. Prototype 30-50%

---
+++ NetworkSolution/Login_Middleware/Database_Request_Handler.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Web.Helpers;
+
+namespace Login_Middleware
+{
+    public class Database_Request_Handler
+    {
+        public string UserDataAddress { get; set; }
+
+        public string Request_Get(DynamicJsonObject data)
+        {
+            string responseOutput = string.Empty;
+
+            dynamic dataObj = data;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(UserDataAddress+$"//{dataObj.UserID}");
+
+            request.Method = "GET";
+
+            HttpWebResponse httpResponse = null;
+
+            try
+            {
+                httpResponse = (HttpWebResponse)request.GetResponse();
+
+                using (Stream responseStream = httpResponse.GetResponseStream())
+                {
+                    if (responseStream != null)
+                    {
+                        using (StreamReader reader = new StreamReader(responseStream))
+                        {
+                            responseOutput = reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"ERROR! Exception Encountered!\nException Message: {e.ToString()}\n TLDR: {e.Message}");
+            }
+            finally
+            {
+                if (httpResponse != null)
+                {
+                    ((IDisposable)httpResponse).Dispose();
+                }
+            }
+
+            return responseOutput;
+        }
+        public void Request_Post(DynamicJsonObject data)
+        {
+            string responseOutput = string.Empty;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(UserDataAddress);
+            request.Method = "POST";
+
+            string dataEncoded = Json.Encode(data);
+
+            byte[] byteArray = Encoding.UTF8.GetBytes(dataEncoded);
+
+            request.ContentType = "application/x-www-form-urlencoded";
+
+            request.ContentLength = byteArray.Length;
+
+        }
+
+        public void Request_Put()
+        {
+
+        }
+
+        public void Request_Delete()
+        {
+
+        }
+
+
+
+    }
+}
---
+++ NetworkSolution/Login_Middleware/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Helpers;
+
+namespace Login_Middleware
+{
+    class Program
+    {
+        static Queue<Database_Request_Handler> databaseRequests = new Queue<Database_Request_Handler>();
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void TCP_Request_Handler(string data)
+        {
+            Database_Request_Handler dbh = databaseRequests.Dequeue();
+            dynamic dataObj = Json.Decode(data);
+
+
+            switch (dataObj.RequestType)
+            {
+                case "Login":
+                    Login(dataObj,dbh);
+                    break;
+                case "Logout":
+                    break;
+                case "Create":
+                    break;
+                case "Delete":
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        static void Request_Token(string data)
+        {
+
+        }
+
+        static string Decrypter(string data)
+        {
+            return data;
+        }
+
+        static string Encrypter(string data)
+        {
+            return data;
+        }
+
+
+        static internal void Login(DynamicJsonObject data, Database_Request_Handler logOnRequest)
+        {
+
+            DynamicJsonObject db_Obj = Json.Decode(logOnRequest.Request_Get(data));
+
+            if (data == db_Obj)
+            {
+                Request_Token(Json.Encode(data));
+            }
+
+        }
+
+        
+    }
+}
