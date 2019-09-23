28bb81cb7ba7262cfe8809ed3fbbe44f2339ff81
Henrik <henrik90@hotmail.dk>
2019-09-24 00:10:52 +0200

    nyeste changes

--- NetworkSolution/Beacon/Beacon.cs
+++ NetworkSolution/Beacon/Beacon.cs
@@ -15,16 +15,30 @@ namespace Beacon
     public class Beacon
     {
         public ServersData serverData = new ServersData();
-        public ConcurrentQueue<TcpClient> servers = new ConcurrentQueue<TcpClient>();
 
         public Beacon()
         {
             serverData.Servers = new List<ServerInformation>();
-            serverData.Servers.Add(new ServerInformation() { ServerName = "Test Server1", IP = "Test IP", Port = 13000 });
 
             Thread t = new Thread(ListenForServers);
             t.IsBackground = true;
             t.Start();
+        }
+
+        public ServersData GetServerData()
+        {
+            List<ServerInformation> temp = new List<ServerInformation>();
+            foreach (var item in serverData.Servers)
+            {
+                if (!Connected(item.Client))
+                    temp.Add(item);
+            }
+            foreach (var item in temp)
+            {
+                serverData.Servers.Remove(item);
+            }
+
+            return serverData;
         }
 
         void ListenForServers()
@@ -34,8 +48,46 @@ namespace Beacon
             while (true)
             {
                 TcpClient client = listener.AcceptTcpClient();
+                lock (serverData)
+                {
+                    serverData.Servers.Add(new ServerInformation()
+                    {
+                        IP = ((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString(),
+                        Port = ((IPEndPoint)client.Client.RemoteEndPoint).Port,
+                        Client = client
+                    }
+                    );
+                }
+                Console.WriteLine("Connected!");
+            }
+        }
 
-                Console.WriteLine("Connected!");           
+        public bool Connected(TcpClient tcpClient)
+        {
+            try
+            {
+                if (tcpClient.Client != null && tcpClient.Client.Connected)
+                {
+                    if (tcpClient.Client.Poll(0, SelectMode.SelectRead))
+                    {
+                        byte[] buff = new byte[1];
+
+                        if (tcpClient.Client.Receive(buff, SocketFlags.Peek) == 0)
+                        {
+                            return false;
+                        }
+
+                        return true;
+                    }
+
+                    return true;
+                }
+
+                return false;
+            }
+            catch
+            {
+                return false;
             }
         }
     }
--- NetworkSolution/Beacon/ServerInformation.cs
+++ NetworkSolution/Beacon/ServerInformation.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Sockets;
 
 namespace Beacon
 {
     public class ServerInformation
     {
-        public string ServerName { get; set; }
         public string IP { get; set; }
         public int Port { get; set; }
+        [JsonIgnore]
+        public TcpClient Client { get; set; }
     }
 
     public class ServersData
