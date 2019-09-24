935dc40ea72a3a4a007ab698ae31b15aee93fb84
Henrik Roemer <henrik90@hotmail.dk>
2019-09-24 12:46:21 +0200

    welp

--- NetworkSolution/UserRestApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace UserRestApi.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- NetworkSolution/UserRestApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace UserRestApi.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
