27ce9bbf0db2d5f72b9dc545792285055a836573
Neebzan <esbenjd@gmail.com>
2019-09-24 14:38:15 +0200

    Added to global variables. Using global variables in MySQL translator

--- NetworkSolution/MySQL translator/User.cs
+++ NetworkSolution/MySQL translator/User.cs
@@ -4,17 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MySQL_translator
-{
-    public enum RequestTypes { Get_User, Create_User, Update_User, Delete_User, Response };
-
-    public enum RequestStatus { Success, AlreadyExists, DoesNotExist, ConnectionError}
+namespace MySQL_translator {
 
     [Serializable]
     public class User
     {
-        public RequestStatus RequestStatus { get; set; }
-        public RequestTypes RequestType { get; set; }
+        public GlobalVariablesLib.RequestStatus RequestStatus { get; set; }
+        public GlobalVariablesLib.RequestTypes RequestType { get; set; }
         public string UserID { get; set; }
         public string PswdHash { get; set; }
         public DateTimeOffset CreatedAt { get; set; }
