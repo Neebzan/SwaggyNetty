8fd912a3062cc7df4820360c06f1740f4da3fda3
Neebzan <esbenjd@gmail.com>
2019-09-25 16:47:21 +0200

    Implemented popup error

--- NetworkSolution/Launcher/MainWindow.xaml.cs
+++ NetworkSolution/Launcher/MainWindow.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -22,19 +23,46 @@ namespace Launcher {
 
         TextBox usernameBox;
         PasswordBox passwordBox;
+        Popup errorPopup;
+        TextBlock errorPopupMessage;
 
         public MainWindow () {
             InitializeComponent();
 
             usernameBox = username_textbox;
             passwordBox = password_textbox;
+            errorPopup = Error_Popup;
+            errorPopupMessage = Error_Popup_Label;
+
+            Deactivated += (object sender, EventArgs e) => {
+                errorPopup.IsOpen = false;
+            };
+
+            LocationChanged += (object sender, EventArgs e) => {
+                errorPopup.IsOpen = false;
+            };
+            this.usernameBox.KeyDown += (object sender, KeyEventArgs e) => {
+                errorPopup.IsOpen = false;
+            };
+
+            errorPopup.IsOpen = false;
         }
 
-        private async void Button_Click (object sender, RoutedEventArgs e) {
-            SecureString password = passwordBox.SecurePassword;
-            string username = usernameBox.Text;
+        private async void Login_Button_Clicked (object sender, RoutedEventArgs e) {
+            if (!string.IsNullOrEmpty(passwordBox.Password) && !string.IsNullOrEmpty(usernameBox.Text)) {
+                SecureString password = passwordBox.SecurePassword;
+                string username = usernameBox.Text;
 
-            await Backend.SendLoginCredentials(username, password);
+                await Backend.SendLoginCredentials(username, password);
+            }
+            else {
+                errorPopup.IsOpen = true;
+                errorPopupMessage.Text = "You must fill out both username and password entries.";
+            }
+        }
+
+        private void Register_Button_Clicked (object sender, RoutedEventArgs e) {
+            
         }
     }
 }
