1eac0427b6a865a81da5c7a7f039edb187cd8bed
Nicolai Ellebæk Bjerg <nico485g@student.eadania.dk>
2019-09-26 16:35:26 +0200

    Can fully login to db, but not token server yet. Can create a user, but no feedback yet

--- NetworkSolution/GlobalVariablesLib/Models/UserModel.cs
+++ NetworkSolution/GlobalVariablesLib/Models/UserModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace GlobalVariablesLib
 {
-    [Serializable]
+    //[Serializable]
     public class UserModel
     {
         public string UserID { get; set; }
