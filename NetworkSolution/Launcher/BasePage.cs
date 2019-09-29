0192bf98bee97cf05085140c4b6eead3c777f471
Esben Dalsgard <esbenjd@gmail.com>
2019-09-29 19:04:41 +0200

    Working on adding animations.. currently broke

---
+++ NetworkSolution/Launcher/BasePage.cs
@@ -0,0 +1,31 @@
+﻿using Launcher.Animations;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Launcher
+{
+    public class BasePage : Page
+    {
+        public PageAnimation PageLoadAnimation { get; set; } = PageAnimation.SlideAndFadeInFromRight;
+        public PageAnimation PageUnloadAnimation { get; set; } = PageAnimation.SlideAndFadeOutToLeft;
+
+        public float SlideSeconds { get; set; } = .8f;
+
+        public BasePage () {
+            if (this.PageLoadAnimation != PageAnimation.None) {
+                this.Visibility = Visibility.Collapsed;
+            }
+
+            this.Loaded += BasePage_Loaded;
+        }
+
+        private void BasePage_Loaded (object sender, RoutedEventArgs e) {
+            AnimateIn();
+        }
+
+        private void AnimateIn () {
+            throw new NotImplementedException();
+        }
+    }
+}
