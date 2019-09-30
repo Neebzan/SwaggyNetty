f0e817bcdf157400619addee1f6504e74ef5a6aa
Neebzan <esbenjd@gmail.com>
2019-09-30 11:42:51 +0200

    jhkæjkoæ

---
+++ NetworkSolution/Launcher/Backend.cs
@@ -0,0 +1,136 @@
+﻿using GlobalVariablesLib;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Launcher
+{
+    public static class Backend {
+        private static string middlewareIP = "10.131.69.129";
+        private static int middlewarePort = 13010;
+        public static UserModel loggedUser { get; private set; }
+
+        static Backend () {
+            loggedUser = new UserModel() {
+                UserID = "Nebberen"
+            };
+        }
+
+        public static async Task<bool> SendLoginCredentials (string username, SecureString password) {
+            TcpClient client = new TcpClient();
+
+            string unsecurePassword = ConvertToUnsecureString(password);
+            string hashedPassword = GetPasswordHash(unsecurePassword);
+
+            try {
+
+                await client.ConnectAsync(middlewareIP, middlewarePort);
+            }
+            catch (Exception) {
+                return false;
+            }
+
+
+            GlobalVariablesLib.UserModel user = new GlobalVariablesLib.UserModel() { UserID = username, PswdHash = hashedPassword, RequestType = GlobalVariablesLib.RequestTypes.Get_User };
+
+            byte [ ] msg = TcpHelper.MessageFormatter.MessageBytes<GlobalVariablesLib.UserModel>(user);
+
+            try {
+                client.GetStream().Write(msg, 0, msg.Length);
+            }
+            catch (Exception) {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static void Logout () {
+            loggedUser = null;
+        }
+
+        public static async Task<bool> SendRegisterRequest (string username, SecureString password) {
+            TcpClient client = new TcpClient();
+
+            string unsecurePassword = ConvertToUnsecureString(password);
+            string hashedPassword = GetPasswordHash(unsecurePassword);
+
+            try {
+
+                await client.ConnectAsync(middlewareIP, middlewarePort);
+            }
+            catch (Exception) {
+                return false;
+            }
+
+            GlobalVariablesLib.UserModel user = new GlobalVariablesLib.UserModel() { UserID = username, PswdHash = hashedPassword, RequestType = GlobalVariablesLib.RequestTypes.Create_User };
+
+            byte [ ] msg = TcpHelper.MessageFormatter.MessageBytes<GlobalVariablesLib.UserModel>(user);
+
+            try {
+                client.GetStream().Write(msg, 0, msg.Length);
+            }
+            catch (Exception) {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static void LaunchGame () {
+            Process.Start("F:/Steam/steamapps/common/Cube World/cubeworld.exe");
+        }
+
+        private static string GetPasswordHash (string password) {
+
+            using (HMACSHA512 t = new HMACSHA512(Encoding.UTF8.GetBytes(password))) {
+                byte [ ] hash;
+                hash = t.ComputeHash(Encoding.UTF8.GetBytes(password));
+                password = BitConverter.ToString(hash).Replace("-", "");
+            }
+
+            return password;
+        }
+        private static string ConvertToUnsecureString (SecureString securePassword) {
+            if (securePassword == null) {
+                return string.Empty;
+            }
+
+            IntPtr unmanagedString = IntPtr.Zero;
+            try {
+                unmanagedString = Marshal.SecureStringToGlobalAllocUnicode(securePassword);
+                return Marshal.PtrToStringUni(unmanagedString);
+            }
+            finally {
+                Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
+            }
+        }
+
+        private static void ReadForAnswer () {
+            //TcpHelper.MessageFormatter.ReadForMessages(new NetworkStre));
+        }
+
+        public static bool CheckPassUniformity (SecureString _password, SecureString _confirmPass) {
+            string pass = ConvertToUnsecureString(_password);
+            string confirmPass = ConvertToUnsecureString(_confirmPass);
+
+            if (pass == confirmPass) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+
+    }
+}
---
+++ NetworkSolution/Launcher/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Launcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Frame mainFrame;
+        public Button playButton;
+
+        public MainWindow () {
+
+            InitializeComponent();
+
+            mainFrame = frame;
+            playButton = play_button;
+            playButton.IsEnabled = false;
+            playButton.Opacity = .5;
+            //this.Content = new LoginPage();
+            mainFrame.NavigationService.Navigate(new LoginPage());
+        }
+    }
+}
