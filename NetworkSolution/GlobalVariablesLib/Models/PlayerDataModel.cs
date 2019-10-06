18a0d368f6cdda68414c5f50fc68065cdeec4a81
Esben Dalsgard <esbenjd@gmail.com>
2019-10-06 16:10:42 +0200

    Added project, updated model

--- NetworkSolution/GlobalVariablesLib/Models/PlayerDataModel.cs
+++ NetworkSolution/GlobalVariablesLib/Models/PlayerDataModel.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace GlobalVariablesLib.Models {
 
+    public enum PlayerDataRequest { Create, Read, Update, Delete};
+    public enum PlayerDataStatus { Success, Failure};
+
    public class PlayerDataModel {
         public string UserID { get; set; }
         public float PositionX { get; set; }
         public float PositionY { get; set; }
         public bool Online { get; set; }
+        public PlayerDataRequest PlayerDataRequest { get; set; }
+        public PlayerDataStatus  PlayerDataStatus { get; set; }
     }
 }
