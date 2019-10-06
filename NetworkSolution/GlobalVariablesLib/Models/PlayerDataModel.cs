76a747fd3b350135dfc3594e65a6167cd3343948
Esben Dalsgard <esbenjd@gmail.com>
2019-10-06 16:06:20 +0200

    Added gamedb translator & datamodel

---
+++ NetworkSolution/GlobalVariablesLib/Models/PlayerDataModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalVariablesLib.Models {
+
+   public class PlayerDataModel {
+        public string UserID { get; set; }
+        public float PositionX { get; set; }
+        public float PositionY { get; set; }
+        public bool Online { get; set; }
+    }
+}
