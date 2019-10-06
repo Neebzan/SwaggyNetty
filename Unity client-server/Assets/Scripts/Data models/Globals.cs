f51d2f4948ed11bcb6279678fa3aea616b81b269
Henrik <henrik90@hotmail.dk>
2019-10-06 17:12:34 +0200

    unity ip change

--- Unity client-server/Assets/Scripts/Data models/Globals.cs
+++ Unity client-server/Assets/Scripts/Data models/Globals.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public static class Globals
 {
-    //public static string MIDDLEWARE_IP = "178.155.161.248";
-    public static string MIDDLEWARE_IP = "127.0.0.1";
+    public static string MIDDLEWARE_IP = "178.155.161.248";
+    //public static string MIDDLEWARE_IP = "127.0.0.1";
     public static int TOKENSYSTEM_PORT = 13005;
 
 
