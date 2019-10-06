e3897c89db90012b6fcb13c820c756369b4dc4c0
Esben Dalsgard <esbenjd@gmail.com>
2019-10-06 16:12:06 +0200

    Update PlayerDataModel.cs

--- NetworkSolution/GlobalVariablesLib/Models/PlayerDataModel.cs
+++ NetworkSolution/GlobalVariablesLib/Models/PlayerDataModel.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace GlobalVariablesLib.Models {
 
     public enum PlayerDataRequest { Create, Read, Update, Delete};
-    public enum PlayerDataStatus { Success, Failure};
+    public enum PlayerDataStatus { None, Success, Failure};
 
    public class PlayerDataModel {
         public string UserID { get; set; }
