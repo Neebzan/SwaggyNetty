285f6640b24903bce25a11ef5dce5b216e0f9c13
Henrik <henrik90@hotmail.dk>
2019-10-07 13:30:33 +0200

    Jens og cam fix

--- Unity client-server/Assets/Scripts/LocalClient.cs
+++ Unity client-server/Assets/Scripts/LocalClient.cs
@@ -22,10 +22,10 @@ public class LocalClient : MonoBehaviour
     void Start()
     {
 
-        //string[] args = Environment.GetCommandLineArgs();
-        //token = args[1];
+        string[] args = Environment.GetCommandLineArgs();
+        token = args[1];
         //GameObject.Find("TokenText").GetComponent<Text>().text = token;
-        token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJKV1RQYXlsb2FkIjoie1wiU2VydmVyc0luZm9cIjp7XCJTZXJ2ZXJzXCI6W119LFwiVXNlcklEXCI6XCJKZW5zXCJ9IiwibmJmIjoxNTcwMTE5MjkwLCJleHAiOjE1NzA1NTEyOTAsImlhdCI6MTU3MDExOTI5MH0.L31Fkm8kaOpVoglhgEv_GvCAD6b1ep0h56OstUnF0d4";
+        //token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJKV1RQYXlsb2FkIjoie1wiU2VydmVyc0luZm9cIjp7XCJTZXJ2ZXJzXCI6W119LFwiVXNlcklEXCI6XCJKZW5zXCJ9IiwibmJmIjoxNTcwMTE5MjkwLCJleHAiOjE1NzA1NTEyOTAsImlhdCI6MTU3MDExOTI5MH0.L31Fkm8kaOpVoglhgEv_GvCAD6b1ep0h56OstUnF0d4";
 
         int port = 13000;
 
